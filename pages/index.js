9a6bfaedc900b93607319d8c9e7497ae944a7eec
coreyr <coreystephenr@gmail.com>
2023-02-15 21:19:05 +1100

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { getProductsInCollection } from "../lib/shopify.js"
 import ProductList from "../components/ProductList"
+import Hero from '../components/Hero'
 
 export default function Home({ products }) {
 
   return (
-    <div className='text-4xl'>
+    <div className=''>
+      <Hero />
       <ProductList products={products} />
     </div>
   )
