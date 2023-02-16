eced50c31d8f2ea4b7bf05dc49c9f1029be8fbe1
coreyr <coreystephenr@gmail.com>
2023-02-16 14:09:32 +1100

    SEO commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import { getProductsInCollection } from "../lib/shopify.js"
 import ProductList from "../components/ProductList"
 import Hero from '../components/Hero'
+import Head from 'next/head'
 
 export default function Home({ products }) {
 
   return (
     <div className=''>
+      <Head>
+        <title>My Next.js Ecommerce Store</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <meta name="description" content="Modern solution to a headless Shopify store built in Next.js" />
+
+      </Head>
       <Hero />
       <ProductList products={products} />
     </div>
